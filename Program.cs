5920359866bc9d2dc1c8959aaa91edda2ddde86e
Kauã <Kau.lira@yahoo.com>
2022-03-25 21:21:07 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome, sobrenomeM; 
+
+Console.Write("digite seu nome:");
+nome = Console.ReadLine()!;
+
+Console.Write("digite seu sobrenome:");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeM = sobrenome.ToUpper();
+
+Console.WriteLine($"nome completo:{nome} {sobrenome}");
+Console.WriteLine($"nome em catálogo:{sobrenomeM}, {nome}");
